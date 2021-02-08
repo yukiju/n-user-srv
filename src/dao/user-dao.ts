a0bca00d9c2e6712e3cc144908483471cd276f0f
milton-reyes <altf4616@outlook.com>
2021-02-08 06:23:47 -0500

    adding user-dao

--- src/dao/user-dao.ts
+++ src/dao/user-dao.ts
@@ -1,1 +1,15 @@
+import { User, UserRow } from '../model/user';
 import { db } from './db';
+
+export function getAllUsers(): Promise<User[]> {
+    const sql = 'SELECT * FROM users ORDER BY id';
+
+    return db.query<UserRow>(sql, []).then(res => {
+        const rows: UserRow[] = res.rows;
+        const users: User[] = rows.map(row => User.from(row));
+        return users;
+    }).catch(e => {
+        console.log(e);
+        return undefined;
+    });
+}
